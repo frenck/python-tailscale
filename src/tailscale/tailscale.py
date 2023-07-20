5a222eb41667d91757361fe61a0039292b6da96b
Clayton Rosenthal <crosenthal@covariant.ai>
2023-07-19 23:58:54 -0700

    new-feature: oauth support

--- src/tailscale/tailscale.py
+++ src/tailscale/tailscale.py
@@ -3,13 +3,13 @@
 from __future__ import annotations
 
 import asyncio
+import json
 import socket
 from dataclasses import dataclass
 from typing import Any, Self
 
-from aiohttp import BasicAuth
 from aiohttp.client import ClientError, ClientResponseError, ClientSession
-from aiohttp.hdrs import METH_GET
+from aiohttp.hdrs import METH_GET, METH_POST
 from yarl import URL
 
 from .exceptions import (
@@ -19,18 +19,72 @@ from .exceptions import (
 )
 from .models import Device, Devices
 
+# Placeholder value for the access token when it is not yet set.
+ACCESS_TOKEN_PENDING = "<pending>"  # noqa: S105
+
 
 @dataclass
 class Tailscale:
     """Main class for handling connections with the Tailscale API."""
 
-    tailnet: str
-    api_key: str
+    # tailnet of '-' is the default tailnet of the API key
+    tailnet: str = "-"
+    api_key: str = ""  # nosec
+    oauth_client_id: str = ""  # nosec
+    oauth_client_secret: str = ""  # nosec
 
     request_timeout: int = 8
     session: ClientSession | None = None
 
     _close_session: bool = False
+
+    async def _check_access(self) -> None:
+        """Initialize the Tailscale client.
+
+        Raises:
+            TailscaleAuthenticationError: when neither api_key nor oauth_client_id and
+                oauth_client_secret are provided.
+
+        """
+        if (
+            not self.api_key
+            and not self.oauth_client_id
+            and not self.oauth_client_secret
+        ):
+            msg = "Either api_key or oauth client is required"
+            raise TailscaleAuthenticationError(msg)
+        if not self.api_key:
+            self.api_key = ACCESS_TOKEN_PENDING
+            self.api_key = await self._get_oauth_token()
+
+    async def _get_oauth_token(self) -> str:
+        """Get an OAuth token from the Tailscale API.
+
+        Raises:
+            TailscaleAuthenticationError: when access key not found in response.
+
+        Returns:
+            A string with the OAuth token, or nothing on error
+
+        """
+        # Tailscale's OAuth endpoint requires form-encoded body
+        # with client_id and client_secret
+        data = {
+            "client_id": self.oauth_client_id,
+            "client_secret": self.oauth_client_secret,
+        }
+        response = await self._request(
+            "oauth/token",
+            data=data,
+            method=METH_POST,
+            use_form_encoding=True,
+        )
+
+        token = json.loads(response).get("access_token", "")
+        if not token:
+            msg = "Failed to get OAuth token"
+            raise TailscaleAuthenticationError(msg)
+        return str(token)
 
     async def _request(
         self,
@@ -38,6 +92,7 @@ class Tailscale:
         *,
         method: str = METH_GET,
         data: dict[str, Any] | None = None,
+        use_form_encoding: bool = False,
     ) -> str:
         """Handle a request to the Tailscale API.
 
@@ -66,9 +121,15 @@ class Tailscale:
         """
         url = URL("https://api.tailscale.com/api/v2/").join(URL(uri))
 
-        headers = {
+        await self._check_access()
+
+        headers: dict[str, str] = {
             "Accept": "application/json",
         }
+
+        if self.api_key and self.api_key != ACCESS_TOKEN_PENDING:
+            # API keys and oauth tokens can use Bearer authentication
+            headers["Authorization"] = f"Bearer {self.api_key}"
 
         if self.session is None:
             self.session = ClientSession()
@@ -76,12 +137,13 @@ class Tailscale:
 
         try:
             async with asyncio.timeout(self.request_timeout):
+                # Use form encoding for OAuth token requests, JSON for others
                 response = await self.session.request(
                     method,
                     url,
-                    json=data,
-                    auth=BasicAuth(self.api_key),
-                    headers=headers,
+                    headers=headers if headers else None,
+                    data=data if use_form_encoding else None,
+                    json=data if not use_form_encoding else None,
                 )
                 response.raise_for_status()
         except asyncio.TimeoutError as exception:
